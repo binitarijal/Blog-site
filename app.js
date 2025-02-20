ce6b513283d434fa381a4c48ac98d14b6ca7409a
Binita rijal <rijalbinita01@gmail.com>
2025-02-20 17:02:29 +0545

    created API

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ require("./model/index")
 
 app.set("view engine","ejs")
 
+app.use(express.urlencoded({extended : true})) //imp for every file to get data from html form
+//app.use(express.json())----imp one
 app.get("/",(req,res)=>{
     res.render("home")
 })
 app.get("/about",(req,res)=>{
     res.render("about")
+})
+app.get("/addBlog",(req,res)=>{
+    res.render("addBlog")
+})
+
+app.post("/addBlog",(req,res)=>{
+    console.log(req.body);
 })
 
 const PORT=2000;
--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "",
-    DB: "105fproject",
+    DB: "104fproject",
     dialect: "mysql",
     pool: {
       max: 5,
